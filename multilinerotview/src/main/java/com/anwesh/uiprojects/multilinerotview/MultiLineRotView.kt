3e1fa4e1e0c7946bbfec9eda650fb10318ee6ab4
Anwesh43 <anweshthecool0@gmail.com>
2019-07-05 08:02:05 +0530

    adding MultiLineRotView class

--- multilinerotview/src/main/java/com/anwesh/uiprojects/multilinerotview/MultiLineRotView.kt
+++ multilinerotview/src/main/java/com/anwesh/uiprojects/multilinerotview/MultiLineRotView.kt
@@ -64,3 +64,21 @@ fun Canvas.drawMLRNode(i : Int, scale : Float, paint : Paint) {
     drawRotLines(sc2, sweepDeg2, size, paint)
     restore()
 }
+
+class MultiLineRotView(ctx : Context) : View(ctx) {
+
+    private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
