08c16ccc483f63151734901a35e0f65d665f2812
Anwesh43 <anweshthecool0@gmail.com>
2019-07-05 08:09:13 +0530

    adding MLRNode class

--- multilinerotview/src/main/java/com/anwesh/uiprojects/multilinerotview/MultiLineRotView.kt
+++ multilinerotview/src/main/java/com/anwesh/uiprojects/multilinerotview/MultiLineRotView.kt
@@ -125,8 +125,52 @@ class MultiLineRotView(ctx : Context) : View(ctx) {
 
         fun stop() {
             if (animated) {
-                animated = false 
+                animated = false
             }
+        }
+    }
+
+    data class MLRNode(var i : Int, val state : State = State()) {
+
+        private var next : MLRNode? = null
+        private var prev : MLRNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < nodes - 1) {
+                next = MLRNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawMLRNode(i, state.scale, paint)
+            next?.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            state.update {
+                cb(i, it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : MLRNode {
+            var curr : MLRNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this 
         }
     }
 }
