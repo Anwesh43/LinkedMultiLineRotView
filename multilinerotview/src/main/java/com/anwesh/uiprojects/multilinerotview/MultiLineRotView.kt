cf47b270b0c29d4cc766f8e51f68fb7df4b07d1a
Anwesh43 <anweshthecool0@gmail.com>
2019-07-05 07:50:56 +0530

    adding Int and Float extension methods

--- multilinerotview/src/main/java/com/anwesh/uiprojects/multilinerotview/MultiLineRotView.kt
+++ multilinerotview/src/main/java/com/anwesh/uiprojects/multilinerotview/MultiLineRotView.kt
@@ -22,3 +22,15 @@ val foreColor : Int = Color.parseColor("#1A237E")
 val backColor : Int = Color.parseColor("#BDBDBD")
 val sweepDeg1 : Float = 45f
 val sweepDeg2 : Float = 90f
+
+fun Int.inverse() : Float = 1f / this
+fun Float.scaleFactor() : Float = Math.floor(this / scDiv).toFloat()
+fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
+fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+fun Float.mirrorValue(a : Int, b : Int) : Float {
+    val k : Float = scaleFactor()
+    return (1 - k) * a.inverse() + k * b.inverse()
+}
+fun Float.updateValue(dir : Float, a : Int, b : Int) : Float {
+    return mirrorValue(a, b) * dir * scGap
+}
