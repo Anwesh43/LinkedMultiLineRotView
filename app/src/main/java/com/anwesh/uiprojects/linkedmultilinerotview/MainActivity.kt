e69e0ea3486af3e30f602f7cd901038cdc7a4ef2
Anwesh43 <anweshthecool0@gmail.com>
2019-07-05 08:18:18 +0530

    adding view in acgtivity

--- app/src/main/java/com/anwesh/uiprojects/linkedmultilinerotview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedmultilinerotview/MainActivity.kt
@@ -2,11 +2,12 @@ package com.anwesh.uiprojects.linkedmultilinerotview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.multilinerotview.MultiLineRotView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        MultiLineRotView.create(this)
     }
 }
