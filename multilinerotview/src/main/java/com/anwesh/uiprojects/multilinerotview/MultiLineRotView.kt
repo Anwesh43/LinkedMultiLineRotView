75ac0d4b192c6658851d7d52a798de293bf1366f
Anwesh43 <anweshthecool0@gmail.com>
2019-07-05 07:47:09 +0530

    adding constants and libraries

--- multilinerotview/src/main/java/com/anwesh/uiprojects/multilinerotview/MultiLineRotView.kt
+++ multilinerotview/src/main/java/com/anwesh/uiprojects/multilinerotview/MultiLineRotView.kt
@@ -3,3 +3,22 @@ package com.anwesh.uiprojects.multilinerotview
 /**
  * Created by anweshmishra on 05/07/19.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.content.Context
+import android.app.Activity
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+
+val nodes : Int = 5
+val lines : Int = 2
+val scGap : Float = 0.05f
+val scDiv : Double = 0.51
+val strokeFactor : Int = 90
+val sizeFactor : Float = 2.9f
+val foreColor : Int = Color.parseColor("#1A237E")
+val backColor : Int = Color.parseColor("#BDBDBD")
+val sweepDeg1 : Float = 45f
+val sweepDeg2 : Float = 90f
